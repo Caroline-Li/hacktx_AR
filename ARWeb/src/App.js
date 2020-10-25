aff7b941efa26b45d6c9ed73d4619737f063246d
Caroline Li <caroline.li.atx@gmail.com>
2020-10-24 19:47:47 -0500

    embedded AR!

--- ARWeb/src/App.js
+++ ARWeb/src/App.js
@@ -4,7 +4,41 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <head>
+        <title>Ebmedded WebAR Experience through echoAR</title>
+        <style>
+            background {
+                top: 0;
+                left: 0;
+                position: fixed;
+                width: 100%;
+                height: 100%;
+                background-color: rgb(0, 45, 100);
+                z-index: -1;
+            },
+            h1 {
+                position: relative;
+                color: white;
+                text-align: center;
+                font-size: 5vh;
+		            font-family: Arial, Helvetica, sans-serif;
+            }
+            iframe {
+                position: relative;
+                width: 100%;
+                height: 75vh;
+            }
+        </style>
+    </head> */}
+    <body>
+	    <div id="background"></div>
+        <h1>Ebmedded WebAR Experience through echoAR</h1>
+          <iframe
+            src="https://console.echoAR.xyz/webar?key=shrill-surf-7760&entry=376f72fb-4fd3-4192-8bd2-aa3e875297e7"
+            title="echoAR WebAR iframe element">
+          </iframe>
+    </body>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +51,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
